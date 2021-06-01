ebbe460ed6be8c2f90decd03195d3164dcf80248
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-06-01 22:39:00 +0700

    Create 5Jenis.java

---
+++ 5Jenis.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+class Jenis extends Toko{
+    void jenis(){
+       System.out.println("Jenis Sepatu = "+Jenis);
+       
+   }
+}
