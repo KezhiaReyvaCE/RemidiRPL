db4d4b4eadc1de7c2e0bc27faae0349c432d9bf7
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-06-01 22:33:52 +0700

    Create Toko.java

---
+++ Toko.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Toko {
+    String name;
+    String Size;
+    String warna;
+    String Jenis;
+    String Kategori;
+    int jumlah;
+    int Harga;
+    
+    
+    void name(){
+        System.out.println("Toko Sepatu Jaya Baru");
+        System.out.println(" ");
+        System.out.println("Nama Kasir = Kezhia Reyva");
+        System.out.println("Tanggal Pembelian = 25 Mei 2021");
+        System.out.println("----------------------------------");
+        System.out.println("Nama Barang yang Dibeli:");
+        System.out.println(" ");
+    }
+    int Harga(){
+        return Harga;
+    }
+  
+    
+}
