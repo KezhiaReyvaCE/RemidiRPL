a5aaa14182c09ddeb35b135a3b52dc18adef491c
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-06-01 22:38:26 +0700

    Create 4Warna.java

---
+++ 4Warna.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+class Warna extends Toko {
+    void warna(){
+       System.out.println("Warna Sepatu = "+warna);
+       
+   }
+}
