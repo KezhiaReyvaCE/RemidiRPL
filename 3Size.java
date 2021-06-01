d406963156fd018926d23f01ec6e38d76377e041
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-06-01 22:37:54 +0700

    Create 3Size.java

---
+++ 3Size.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+ class Size extends Toko{
+  
+   void Size(){
+       System.out.println("Ukuran Sepatu = "+Size);
+       
+   }
+    
+}
